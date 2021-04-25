d6f8546fefd301dfc5c721429e0b57ec9077c8b4
nathaliefons <nathalie.fons@gmail.com>
2021-04-25 20:01:11 +0200

    maj dossier

--- Projet_Flask/src/form.py
+++
@@ -1,5 +0,0 @@
-
-
-def formulaire():
-    return "<html><head><title>Formulaire utilisateur</title></head> <body> <form><label>Nom:</label><input type=""submit""/></form><form><label>Prénom:</label><input type=""submit""/></form><button type=""button"">Valider</button></body> </html>"
-    return "<html><body></body></html>"
